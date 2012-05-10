8effd01c73025c7c5642a4b242268abace55ad0e
dennyabraham <email@dennyabraham.com>
2012-05-10 18:25:18 -0400

    conditionally require backports

--- lib/gatling_gun.rb
+++ lib/gatling_gun.rb
@@ -5,10 +5,14 @@ require 'time'
 require 'uri'
 require 'ostruct'
 
-require 'gatling_gun/core_ext'
 require 'gatling_gun/api_call'
 require 'gatling_gun/response'
 require 'gatling_gun/client'
+
+# Ruby 1.9 backports
+if RUBY_VERSION.match(/1.8.7/)
+  require 'gatling_gun/core_ext' 
+end
 
 module GatlingGun
   def self.configure
